21780dbb730776ba90d7de7b0e765c66eca867d9
Ade Widyatama Dian Boernama <adewidyatama@gmail.com>
2017-03-26 00:02:44 +0800

    posstest4

--- app/Http/Controllers/RuanganController.php
+++ app/Http/Controllers/RuanganController.php
@@ -10,17 +10,40 @@ use App\Ruangan;
 class RuanganController extends Controller
 {
     public function awal(){
-    	return "Hello dari RuanganController";
+        return view ('ruangan.awal', ['data'=>Ruangan::all()]);
     }
 
     public function tambah(){
-    	return $this->simpan();
+        return view('ruangan.tambah');
     }
 
-    public function simpan(){
-    	$ruangan= new Ruangan();
-    	$ruangan->title='Ruangan 1';
-        $ruangan->save();
-    	return "Data ruangan dengan nama {$ruangan->title} telah disimpan";
+    public function simpan(Requests $input){
+        $ruangan=new Ruangan();
+        $ruangan->title=$input->title;
+        $informasi=$ruangan->save() ? 'Berhasil simpan data' : 'Gagal simpan data';
+        return redirect('ruangan')->with(['informasi'=>$informasi]);
+    }
+
+    public function edit($id){
+        $ruangan=Ruangan::find($id);
+        return view('ruangan.edit')->with(array('ruangan'=>$ruangan));
+    }
+
+    public function lihat($id){
+        $ruangan=Ruangan::find($id);
+        return view('ruangan.lihat')->with(array('ruangan'=>$ruangan));
+    }
+
+    public function update($id, Requests $input){
+        $ruangan=Ruangan::find($id);
+        $ruangan->title=$input->title;
+        $informasi=$ruangan->save() ? 'Berhasil update data' : 'Gagal update data';
+        return redirect('ruangan')->with(['informasi'=>$informasi]);
+    }
+
+    public function hapus($id){
+        $ruangan=Ruangan::find($id);
+        $informasi = $ruangan->delete() ? 'Berhasil menghapus data' : 'Gagal menghapus data';
+        return redirect('ruangan')->with(['informasi'=>$informasi]);
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Requests;
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -10,39 +10,37 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
 Route::get('/', function () {
-    return view('SelamatDatang');
+    return view('welcome');
 });
-
-Route::get('/public', function(){
-	return view('nama');
-});
-
+Route::auth();
+Route::get('/home', 'HomeController@index');
 Route::get('pengguna','PenggunaController@awal');
-
 Route::get('pengguna/tambah','PenggunaController@tambah');
-
-Route::get('Ruangan', 'RuanganController@awal');
-
-Route::get('Ruangan/tambah', 'RuanganController@tambah');
-
-Route::get('Matakuliah', 'MatakuliahController@awal');
-
-Route::get('Matakuliah/tambah', 'MatakuliahController@tambah');
-
-Route::get('Dosen', 'DosenController@awal');
-
-Route::get('Dosen/tambah', 'DosenController@tambah');
-
-Route::get('Mahasiswa', 'MahasiswaController@awal');
-
-Route::get('Mahasiswa/tambah', 'MahasiswaController@tambah');
-
-Route::get('Dosen_Matakuliah', 'Dosen_MatakuliahController@awal');
-
-Route::get('Dosen_Matakuliah/tambah', 'Dosen_MatakuliahController@tambah');
-
-Route::get('Jadwal', 'JadwalController@awal');
-
-Route::get('Jadwal/tambah', 'JadwalController@tambah');
+Route::post('pengguna/simpan','PenggunaController@simpan');
+Route::get('pengguna/edit/{pengguna}','PenggunaController@edit');
+Route::post('pengguna/edit/{pengguna}','PenggunaController@update');
+Route::get('pengguna/lihat/{pengguna}','PenggunaController@lihat');
+Route::get('pengguna/hapus/{pengguna}','PenggunaController@hapus');
+Route::get('ruangan','ruangancontroller@awal');
+Route::get('ruangan/tambah','ruangancontroller@tambah');
+Route::post('ruangan/simpan','ruangancontroller@simpan');
+Route::get('ruangan/edit/{ruangan}','ruangancontroller@edit');
+Route::post('ruangan/edit/{ruangan}','ruangancontroller@update');
+Route::get('ruangan/lihat/{ruangan}','ruangancontroller@lihat');
+Route::get('ruangan/hapus/{ruangan}','ruangancontroller@hapus');
+Route::get('matakuliah','MatakuliahController@awal');
+Route::get('matakuliah/tambah','MatakuliahController@tambah');
+Route::post('matakuliah/simpan','MatakuliahController@simpan');
+Route::get('matakuliah/edit/{matakuliah}','MatakuliahController@edit');
+Route::post('matakuliah/edit/{matakuliah}','MatakuliahController@update');
+Route::get('matakuliah/lihat/{matakuliah}','matakuliahController@lihat');
+Route::get('matakuliah/hapus/{matakuliah}','MatakuliahController@hapus');
+Route::get('dosen','dosencontroller@awal');
+Route::get('dosen/tambah','dosencontroller@tambah');
+Route::get('mahasiswa','mahasiswacontroller@awal');
+Route::get('mahasiswa/tambah','mahasiswacontroller@tambah');
+Route::get('dosen_matakuliah','dosen_matakuliahcontroller@awal');
+Route::get('dosen_matakuliah/tambah','dosen_matakuliahcontroller@tambah');
+Route::get('jadwal_matakuliah','jadwal_matakuliahcontroller@awal');
+Route::get('jadwal_matakuliah/tambah','jadwal_matakuliahcontroller@tambah');
